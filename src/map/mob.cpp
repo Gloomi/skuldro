750b29fbb726aef4666ce4e6fe9b0d6b8e016dd3
Gloomi <eee.mail.ph2@gmail.com>
2023-02-12 14:19:25 +0800

    change rare item announce
    Changed announce from global to map

--- src/map/mob.cpp
+++ src/map/mob.cpp
@@ -2827,7 +2827,8 @@ int mob_dead(struct mob_data *md, struct block_list *src, int type)
 				char message[128];
 				sprintf (message, msg_txt(NULL,541), mvp_sd->status.name, md->name, it->ename.c_str(), (float)drop_rate/100);
 				//MSG: "'%s' won %s's %s (chance: %0.02f%%)"
-				intif_broadcast(message,strlen(message)+1,BC_DEFAULT);
+				//intif_broadcast(message,strlen(message)+1,BC_DEFAULT);
+				clif_broadcast(&sd->bl, message, strlen(message) + 1, BC_DEFAULT, ALL_SAMEMAP);
 			}
 			// Announce first, or else ditem will be freed. [Lance]
 			// By popular demand, use base drop rate for autoloot code. [Skotlex]
@@ -3029,7 +3030,8 @@ int mob_dead(struct mob_data *md, struct block_list *src, int type)
 					char message[128];
 					sprintf (message, msg_txt(NULL,541), mvp_sd->status.name, md->name, i_data->ename.c_str(), temp/100.);
 					//MSG: "'%s' won %s's %s (chance: %0.02f%%)"
-					intif_broadcast(message,strlen(message)+1,BC_DEFAULT);
+					//intif_broadcast(message,strlen(message)+1,BC_DEFAULT);
+					clif_broadcast(&sd->bl, message, strlen(message) + 1, BC_DEFAULT, ALL_SAMEMAP);
 				}
 
 				mob_setdropitem_option(&item, &mdrop[i]);
